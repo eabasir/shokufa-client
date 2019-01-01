6180f9a4bcaaf6ba55d3e3b1091f462d752dec61
eabasir@gmail.com <eabasir@gmail.com>
2019-01-01 23:58:01 +0330

    add js files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shokufa-client';
+
+
+  loadAPI: Promise<any>;
+
+  constructor() {
+    this.loadAPI = new Promise((resolve) => {
+      this.loadScript();
+      resolve(true);
+    });
+  }
+  public loadScript() {
+    let isFound = false;
+    const scripts = document.getElementsByTagName('script');
+    for (let i = 0; i < scripts.length; ++i) {
+      if (scripts[i].getAttribute('src') != null && scripts[i].getAttribute('src').includes('loader')) {
+        isFound = true;
+      }
+    }
+
+    if (!isFound) {
+      const dynamicScripts = [
+        // 'assets/js/jquery-2.2.4.min.js',
+        // 'assets/js/common_scripts.min.js',
+        'assets/js/functions.js',
+      ];
+ 
+      for (let i = 0; i < dynamicScripts.length; i++) {
+        const node = document.createElement('script');
+        node.src = dynamicScripts[i];
+        node.type = 'text/javascript';
+        node.async = false;
+        node.charset = 'utf-8';
+        document.getElementsByTagName('body')[0].appendChild(node);
+      }
+
+    }
+  }
 }
+
+
